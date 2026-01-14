d21b6e87bbd8c341ed8b165abc4ccd7a0502ccd6
xavierfingers <xavierfingers@gmail.com>
2026-01-13 18:19:17 -0700

    Added prime feature

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def sqrt(x, d, g=1.0):
  for i in range(0, 10):
   g = mp.mpf(g + x / g) / 2
  return str(mp.mpf(g))
+def prime(x):
+ is_prime = [True] * (x + 1)
+ is_prime[0] = is_prime[1] = False
+ for i in range(2, int(x**0.5) + 1):
+  if is_prime[i]:
+   for j in range(i * i, x + 1, i):
+    is_prime[j] = False
+ return [i for i in range(2, x + 1) if is_prime[i]]
 print("Welcome to Num-Cruncher!")
 choice = input("Enter choice: ")
 if choice == "gcd":
@@ -25,3 +33,6 @@ if choice == "sqrt":
  n = int(input("Enter number: "))
  d = int(input("Enter digits: "))
  print(f"sqrt = {sqrt(n, d)}")
+if choice == "primes":
+ n = int(input("Enter limit: "))
+ print(f"primes = {prime(n)}") 
